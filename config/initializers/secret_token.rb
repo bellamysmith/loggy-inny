f29780368c3a60b154c1d02f20c62adc8ae462f8
bellamysmith <bellamysmith1@gmail.com>
2014-09-11 09:39:03 -0700

    Adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoggyInny::Application.config.secret_key_base = '64a9efc23fbd181ec00886984fd96794d9ac8da6e79e64adaca74ad0394463246af70ec82dc73b71f89c975183cd8b58ca63151d79fa812623265b7258da2859'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoggyInny::Application.config.session_store :cookie_store, key: '_loggy-inny_session'
